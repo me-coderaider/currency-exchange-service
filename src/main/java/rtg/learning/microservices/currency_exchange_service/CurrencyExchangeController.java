bf1659c0747848239b908a0a491c8778b9b63ffd
me-coderaider <kashyappankajnsp@gmail.com>
2024-08-18 09:12:17 +0530

    hardcoding currency-exchange

---
+++ src/main/java/rtg/learning/microservices/currency_exchange_service/CurrencyExchangeController.java
@@ -0,0 +1,18 @@
+package rtg.learning.microservices.currency_exchange_service;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	@GetMapping(path="/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
+			@PathVariable String to
+			) {
+		return new CurrencyExchange(1000L,from, to, BigDecimal.valueOf(50));
+	}
+}
