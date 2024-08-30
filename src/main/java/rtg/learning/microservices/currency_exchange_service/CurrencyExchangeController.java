9b3887ece2bc3bcd5248feb394b9a0909988420a
me-coderaider <kashyappankajnsp@gmail.com>
2024-08-30 13:15:44 +0530

    Adding logger to see the attached ID on request by Micrometer

--- src/main/java/rtg/learning/microservices/currency_exchange_service/CurrencyExchangeController.java
+++ src/main/java/rtg/learning/microservices/currency_exchange_service/CurrencyExchangeController.java
@@ -1,7 +1,7 @@
 package rtg.learning.microservices.currency_exchange_service;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger=LoggerFactory.getLogger(CurrencyExchangeController.class);
 	
 	@Autowired
 	private CurrencyExchangeRepository repository;
@@ -22,6 +24,9 @@ public class CurrencyExchangeController {
 			@PathVariable String to
 			) {
 //		CurrencyExchange currencyExchange=new CurrencyExchange(1000L,from, to, BigDecimal.valueOf(50));
+		//INFO [currency-exchange-service,284fef1fa4fd94657bedf8b2ac7cbd57,c9325e9ec322df8c] 10564 --- [currency-exchange-service] [nio-8000-exec-1] [284fef1fa4fd94657bedf8b2ac7cbd57-c9325e9ec322df8c] r.l.m.c.CurrencyExchangeController       : retrieveExchangeValue called with USD to INR
+
+		logger.info("retrieveExchangeValue called with {} to {}",from,to);
 		
 		CurrencyExchange currencyExchange=repository.findByFromAndTo(from, to);
 		if(currencyExchange==null) {
