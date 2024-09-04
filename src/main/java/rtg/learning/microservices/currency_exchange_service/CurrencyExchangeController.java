5fdaa9b16fecb30aba67014075860b164cee26e4
me-coderaider <kashyappankajnsp@gmail.com>
2024-09-04 23:19:04 +0530

    kubernetes changes

--- src/main/java/rtg/learning/microservices/currency_exchange_service/CurrencyExchangeController.java
+++ src/main/java/rtg/learning/microservices/currency_exchange_service/CurrencyExchangeController.java
@@ -10,31 +10,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
-	
-	private Logger logger=LoggerFactory.getLogger(CurrencyExchangeController.class);
-	
+
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
 	@Autowired
 	private CurrencyExchangeRepository repository;
-	
+
 	@Autowired
 	private Environment environment;
-	
-	@GetMapping(path="/currency-exchange/from/{from}/to/{to}")
-	public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
-			@PathVariable String to
-			) {
-//		CurrencyExchange currencyExchange=new CurrencyExchange(1000L,from, to, BigDecimal.valueOf(50));
-		//INFO [currency-exchange-service,284fef1fa4fd94657bedf8b2ac7cbd57,c9325e9ec322df8c] 10564 --- [currency-exchange-service] [nio-8000-exec-1] [284fef1fa4fd94657bedf8b2ac7cbd57-c9325e9ec322df8c] r.l.m.c.CurrencyExchangeController       : retrieveExchangeValue called with USD to INR
 
-		logger.info("retrieveExchangeValue called with {} to {}",from,to);
-		
-		CurrencyExchange currencyExchange=repository.findByFromAndTo(from, to);
-		if(currencyExchange==null) {
-			throw new RuntimeException("Unable to find data for "+from+" to "+to);
+	@GetMapping(path = "/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+//		CurrencyExchange currencyExchange=new CurrencyExchange(1000L,from, to, BigDecimal.valueOf(50));
+		// INFO
+		// [currency-exchange-service,284fef1fa4fd94657bedf8b2ac7cbd57,c9325e9ec322df8c]
+		// 10564 --- [currency-exchange-service] [nio-8000-exec-1]
+		// [284fef1fa4fd94657bedf8b2ac7cbd57-c9325e9ec322df8c]
+		// r.l.m.c.CurrencyExchangeController : retrieveExchangeValue called with USD to
+		// INR
+
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
+		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+		if (currencyExchange == null) {
+			throw new RuntimeException("Unable to find data for " + from + " to " + to);
 		}
-		
+
 		String port = environment.getProperty("local.server.port");
-		currencyExchange.setEnvironment(port);
+		// CHANGE-KUBERNETES
+		String host = environment.getProperty("HOSTNAME");
+		String version = "v11";
+
+		currencyExchange.setEnvironment(port + " " + version + " " + host);
 		return currencyExchange;
 	}
 }
